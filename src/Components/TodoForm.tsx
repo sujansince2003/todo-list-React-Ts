57da0b9aa66777751a6bb350174bac069843046a
sujan khatri <sujansince2059@gmail.com>
2024-01-26 13:35:45 +0545

    functionality define for form

--- src/Components/TodoForm.tsx
+++ src/Components/TodoForm.tsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+import { useTodo } from "../Contexts";
+
 function TodoForm() {
+  const [todo, setTodo] = useState("");
+  const { addTodo } = useTodo();
+
+  function addTodoitem(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (!todo) return;
+    addTodo({ todo: todo, completed: false });
+    setTodo("");
+  }
   return (
-    <form className="flex">
+    <form className="flex" onSubmit={(e) => addTodoitem(e)}>
       <input
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
         type="text"
         placeholder="Write Todo..."
         className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
