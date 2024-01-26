8ccf07395d99187936582cf36df988aedec588f3
sujan khatri <sujansince2059@gmail.com>
2024-01-26 13:36:12 +0545

    context defintion with ts props

--- src/Contexts/TodoContext.ts
+++ src/Contexts/TodoContext.ts
@@ -1,36 +1,29 @@
-
-
-import { createContext,useContext } from "react";
-
+import { createContext, useContext } from "react";
 
 export interface Todo {
-   id: number;
-    todo: string;
-    completed: boolean;
-   }
-
-
-export const TodoContext=createContext(
-    {
-todos:[
-    {
-        id:1,
-        todo:"Learn react context",
-        completed:false
-    }
-],
-addTodo:(todo:Todo)=>{},
-updateTodo:(id:number,todo:Todo)=>{},
-deleteTodo:(id:number)=>{},
-toggleTodo:(id:number)=>{},
-    }
-)
-
-export const TodoContextProvider=TodoContext.Provider
-
-export function useTodo()
-{
-    return useContext(TodoContext)
+  id: number;
+  todo: string;
+  completed: boolean;
 }
 
-// eslint-enable no-unused-vars
+interface TodoContextProps {
+  todos: Todo[];
+  addTodo: (todo: Todo) => void;
+  updateTodo: (id: number, todo: Todo) => void;
+  deleteTodo: (id: number) => void;
+  toggleTodo: (id: number) => void;
+}
+
+export const TodoContext = createContext<TodoContextProps>({
+  todos: [],
+  addTodo: () => {},
+  updateTodo: () => {},
+  deleteTodo: () => {},
+  toggleTodo: () => {},
+});
+
+export const TodoContextProvider = TodoContext.Provider;
+
+export function useTodo() {
+  return useContext(TodoContext);
+}
